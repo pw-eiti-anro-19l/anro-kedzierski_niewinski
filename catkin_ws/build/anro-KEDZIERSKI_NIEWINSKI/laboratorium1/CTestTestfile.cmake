b5ccf5cc1784fb7c809aaa0f93aeab1322206dc4
Michał Kędzierski <mkedzie3@puma.robotyka.ia.pw.edu.pl>
2019-03-14 19:19:44 +0100

    Committer: Michał Kędzierski <mkedzie3@puma.robotyka.ia.pw.edu.pl> Z labow

---
+++ catkin_ws/build/anro-KEDZIERSKI_NIEWINSKI/laboratorium1/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mkedzie3/anro-kedzierski_niewinski/catkin_ws/src/anro-KEDZIERSKI_NIEWINSKI/laboratorium1
+# Build directory: /home/mkedzie3/anro-kedzierski_niewinski/catkin_ws/build/anro-KEDZIERSKI_NIEWINSKI/laboratorium1
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
