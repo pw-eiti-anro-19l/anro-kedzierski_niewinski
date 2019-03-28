02a85059072bb5b2276310bb37cc8b3a1978092c
Michał Kędzierski <mkedzie3@puma.robotyka.ia.pw.edu.pl>
2019-03-28 19:03:36 +0100

    Poprawa

--- catkin_ws/src/gadacz/scripts/program.py
+++ catkin_ws/src/gadacz/scripts/program.py
@@ -11,16 +11,16 @@ def talker():
 	message = Twist()
 	while not rospy.is_shutdown():
 		character = click.getchar()
-		if character == 'w':
+		if character == frw_char:
 			message.linear.x  = 1.0
 			message.angular.z = 0.0
-		elif character =='s':
+		elif character ==bwd_char:
 			message.linear.x  = -1.0
 			message.angular.z = 0.0
-		elif character =='a':
+		elif character ==lft_char:
 			message.linear.x  = 0.0
 			message.angular.z = 1.0
-		elif character =='d':
+		elif character ==rth_char:
 			message.linear.x  = 0.0
 			message.angular.z = -1.0
 
@@ -32,6 +32,10 @@ def talker():
 
 if __name__ == '__main__':
 	try:
+		frw_char = rospy.get_param('fwd')
+		bwd_char = rospy.get_param('bwd')
+		lft_char = rospy.get_param('lft')
+		rth_char = rospy.get_param('rth')	
 		talker()
 	except rospy.ROSInterruptException:
 		pass
