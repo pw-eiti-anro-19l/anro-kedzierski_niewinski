46152f1a9d7c237731853d08ead803d886e5fec5
Kuba <jakubniewinski@yahoo.com>
2019-04-09 01:39:53 +0200

    dziala nieruchomy model, dokonczyc state_publisher

--- catkin_ws/src/dh/src/state_publisher.cpp
+++ catkin_ws/src/dh/src/state_publisher.cpp
@@ -10,8 +10,8 @@ int main(int argc, char** argv) {
     tf::TransformBroadcaster broadcaster;
     ros::Rate loop_rate(30);
 
-    const double degree = M_PI/180;
-
+   	const double degree = M_PI/180;
+	double tilt = 0, tinc = degree, swivel=0, angle=0, height=0, hinc=0.005;
     // message declarations
     geometry_msgs::TransformStamped odom_trans;
     sensor_msgs::JointState joint_state;
@@ -24,13 +24,13 @@ int main(int argc, char** argv) {
         joint_state.name.resize(3);
         joint_state.position.resize(3);
         joint_state.name[0] ="link1";
-        joint_state.position[0] = 0;
+        joint_state.position[0] = swivel;
         joint_state.name[1] ="link2";
-        joint_state.position[1] = 0;
+        joint_state.position[1] = tilt;
         joint_state.name[2] ="link3";
-        joint_state.position[2] = 0;
+        joint_state.position[2] = height;
 
-/*
+
         // update transform
         // (moving in a circle with radius=2)
         odom_trans.header.stamp = ros::Time::now();
@@ -50,7 +50,7 @@ int main(int argc, char** argv) {
         if (height>.2 || height<0) hinc *= -1;
         swivel += degree;
         angle += degree/4;
-*/
+
         // This will adjust as needed per iteration
         loop_rate.sleep();
     }
