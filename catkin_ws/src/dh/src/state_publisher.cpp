906b7afa376992cb957f992516a934c2135a9bc9
Kuba <jakubniewinski@yahoo.com>
2019-04-10 18:32:17 +0200

    nowy model

--- catkin_ws/src/dh/src/state_publisher.cpp
+++ catkin_ws/src/dh/src/state_publisher.cpp
@@ -11,16 +11,14 @@ int main(int argc, char** argv) {
     ros::Rate loop_rate(360);
 
    	const double degree = M_PI/180;
-	double angle=0;
+	double angle=0, theta1=0,theta2=0,theta3=0, a2=1;
     // message declarations
    
 
     while (ros::ok()) {
        //update joint_state
 	tf::Quaternion myq;
-	myq.setRPY(0,0,angle); //theta 1
-	geometry_msgs::Quaternion msg;
-	tf::quaternionTFToMsg(myq,msg);
+	
 	//floor->link1
 	geometry_msgs::TransformStamped odom1;
     	sensor_msgs::JointState joint1;
@@ -28,9 +26,12 @@ int main(int argc, char** argv) {
     	odom1.child_frame_id = "link1";
         joint1.header.stamp = ros::Time::now();
 	//sterowanie
+	myq.setRPY(theta1,0,0); //theta 1
+	geometry_msgs::Quaternion msg;
+	tf::quaternionTFToMsg(myq,msg);
 	odom1.transform.translation.x = 0;
         odom1.transform.translation.y = 0;
-        odom1.transform.translation.z = 1;
+        odom1.transform.translation.z =0.5;
 	odom1.transform.rotation=msg;
         odom1.header.stamp = ros::Time::now();
         //send the joint state and transform
@@ -44,12 +45,12 @@ int main(int argc, char** argv) {
     	odom2.child_frame_id = "link2";
         joint2.header.stamp = ros::Time::now();
 	//sterowanie
-	myq.setRPY(-1.57,0,angle); //theta 2
+	myq.setRPY(-1.57,0,theta2); //theta 2
 	tf::quaternionTFToMsg(myq,msg);
 	odom2.transform.rotation=msg;
 	odom2.transform.translation.x = 0;
-        odom2.transform.translation.y = 0;
-        odom2.transform.translation.z = 0;
+        odom2.transform.translation.y = 0.5;
+        odom2.transform.translation.z = 0.5;
         odom2.header.stamp = ros::Time::now();
         //send the joint state and transform
         joint_pub.publish(joint2);
@@ -60,12 +61,12 @@ int main(int argc, char** argv) {
     	sensor_msgs::JointState joint3;
     	odom3.header.frame_id = "link2";
     	odom3.child_frame_id = "link3";
-	myq.setRPY(0,0,angle); //theta 3
+	myq.setRPY(theta3,0,0); //theta 3
 	tf::quaternionTFToMsg(myq,msg);
 	odom3.transform.rotation=msg;
 	odom3.transform.translation.x = 0;
         odom3.transform.translation.y = 0;
-        odom3.transform.translation.z = 0.5+sin(angle)/4; //zamienic na a2
+        odom3.transform.translation.z = a2; //zamienic na a2
         joint3.header.stamp = ros::Time::now();
 	//sterowanie
 	//odom3.transform.rotation=msg;
