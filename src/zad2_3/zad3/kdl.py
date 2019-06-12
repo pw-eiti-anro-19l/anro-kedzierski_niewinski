f9bdd7c52d5801da2291c0f56608360c24c3cb84
jniewinski <jakubniewinski@yahoo.com>
2019-06-12 17:26:52 +0200

    uporzadkowanie zadan

---
+++ src/zad2_3/zad3/kdl.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+import rospy
+import yaml
+import PyKDL as kdl
+from sensor_msgs.msg import *
+from geometry_msgs.msg import *
+from tf.transformations import *
+from visualization_msgs.msg import Marker
+
+
+def callback(data):
+    marker=PoseStamped()
+    kdlChain =kdl.Chain()   
+    frame = kdl.Frame();
+    main_matrix = translation_matrix((0, 0, 0))
+    global path
+    infile = yaml.load(open(path+"/src/zad/zad3/plik.yaml"))
+    dh = infile["dh"]
+    kdlChain.addSegment(kdl.Segment(kdl.Joint(kdl.Joint.RotZ), frame.DH(0, 0, dh[0][4] , 0)))
+  		
+    kdlChain.addSegment(kdl.Segment(kdl.Joint(kdl.Joint.RotX), frame.DH(dh[1][4], 0, 0, dh[1][2])))
+    	
+    kdlChain.addSegment(kdl.Segment(kdl.Joint(kdl.Joint.RotY), frame.DH(dh[2][4], 0, 0, 0)))
+
+    jointPos = kdl.JntArray(kdlChain.getNrOfJoints())
+    jointPos[0] = data.position[0] 
+    jointPos[1] = data.position[1]
+    jointPos[2] = data.position[2]
+    
+    forvKin = kdl.ChainFkSolverPos_recursive(kdlChain)
+    eeFrame = kdl.Frame()
+    forvKin.JntToCart(jointPos, eeFrame)
+
+
+
+    quaternion = eeFrame.M.GetQuaternion()
+
+    marker.header.frame_id = 'floor'
+    marker.header.stamp = rospy.Time.now()
+
+
+    marker.pose.position.x = eeFrame.p[0]
+    marker.pose.position.y = eeFrame.p[1]
+    marker.pose.position.z = eeFrame.p[2]
+
+    marker.pose.orientation.x = quaternion[0]
+    marker.pose.orientation.y = quaternion[1]
+    marker.pose.orientation.z = quaternion[2]
+    marker.pose.orientation.w = quaternion[3]
+    print(quaternion)
+   
+    publisher.publish(marker)
+
+def kdl_listener():
+    rospy.init_node('KDL_DKIN', anonymous = False)
+
+    rospy.Subscriber("joint_states", JointState , callback)
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    path = rospy.get_param("path")
+    path = path.replace('\n', '')
+    t_list = {}
+
+    publisher = rospy.Publisher('kdl', PoseStamped, queue_size=100)
+
+    try:
+	    kdl_listener()        
+    except rospy.ROSInterruptException:
+        pass
---
+++ src/zad2_3/zad3/nonkdl.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+import rospy
+import yaml
+from sensor_msgs.msg import *
+from geometry_msgs.msg import *
+from tf.transformations import *
+from visualization_msgs.msg import Marker
+
+def callback(data):
+    marker=PoseStamped()
+    main_matrix = translation_matrix((0, 0, 0))
+    global path
+    infile = yaml.load(open(path+"/src/zad/zad3/wynik.yaml"))
+    j0 = infile["j0"]
+    j1 = infile["j1"]
+    j2 = infile["j2"]
+    j=[j0,j1,j2]
+
+
+    i=0
+    while i<3:
+        th=j[i]['len']
+        x=j[i]['z']
+        if i==2:
+            b=-1
+        else:
+            b=1
+        if i==1:
+            p=0.1
+        else:
+            p=0
+        if i==0:
+            a=1
+        else:
+            a=0
+
+        matrix_a = translation_matrix((0, 0, a))
+        matrix_al = rotation_matrix(j[i]['r'], (0, 1, 0))
+        matrix_d= translation_matrix((0, x*th, 0))
+        matrix_th = rotation_matrix(data.position[i], (0, 0, b))
+        trans = concatenate_matrices(matrix_a,matrix_al, matrix_th,matrix_d)
+        t_list[i] = trans
+        #print(i)
+        #print(trans)
+        i += 1
+
+    main_matrix = concatenate_matrices(t_list[0], t_list[1], t_list[2])
+
+
+    x, y, z = translation_from_matrix(main_matrix)
+
+
+    
+    xq, yq, zq, wq = quaternion_from_matrix(main_matrix)
+    print(quaternion_from_matrix(main_matrix))
+    marker.header.frame_id = 'floor'
+    marker.header.stamp = rospy.Time.now()
+
+
+    marker.pose.position.x = x
+    marker.pose.position.y = y
+    marker.pose.position.z = z
+
+    marker.pose.orientation.x = xq
+    marker.pose.orientation.y = -zq
+    marker.pose.orientation.z = yq
+    marker.pose.orientation.w = wq
+
+   
+    publisher.publish(marker)
+
+def nonkdl_listener():
+    rospy.init_node('NONKDL_DKIN', anonymous = False)
+
+    rospy.Subscriber("joint_states", JointState , callback)
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    path = rospy.get_param("path")
+    path = path.replace('\n','')
+    t_list = {}
+    publisher = rospy.Publisher('nonkdl', PoseStamped, queue_size=10)
+
+    try:
+
+	    nonkdl_listener()
+    except rospy.ROSInterruptException:
+
+            pass
